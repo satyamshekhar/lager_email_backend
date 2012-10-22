7554e693f612ce6a4d16e58562b9f598175cbfa1
Satyam Shekhar <satyamshekhar@gmail.com>
2012-10-22 01:38:00 -0700

    Updating year in license.

--- src/lager_email_backend.erl
+++ src/lager_email_backend.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2011 by Satyam Shekhar. All Rights Reserved.
+%% Copyright (c) 2012 by Satyam Shekhar. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
